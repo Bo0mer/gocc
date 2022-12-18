edf673fd7c09c7e3fcb0b635d904d5544f3dace3
Ivan Borshukov <iborshukov@plandelta.com>
2022-12-18 17:33:41 +0200

    Add slides from first lecture

---
+++ code/intro/strings_and_runes.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("We have a cool fmt package: %c %c %c!\n", '\u2713', '☢', '😎')
+	fmt.Printf("こんにちは世界!\n")
+	fmt.Printf(`With backticks we can ignore backslashes like \n!`)
+}
---
+++ code/intro/wrong_type_inference.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+name := "Асен"
+
+func main() {
+	fmt.Printf("%s, защо да не може?\n", name)
+}
